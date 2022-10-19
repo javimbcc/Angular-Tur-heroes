013d2d8338cac587f584c9e39e93331482087481
JAVIER\menaj <menajavier2002@gmail.com>
2022-10-19 11:39:28 +0200

    añadido nuevo componente para los heroes detais y un servicio para poder recoger los heroes desde un serviico y no desde un html para que no sea accesible

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,26 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../interfaces/heroeInterface';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+
+
   hero: Hero = {
     id: 1,
-    name: 'Windstorm'
+    name: 'Windstorm',
+    power:3483284,
+    realname: 'nefwaf'
   };
 
-  heroes = HEROES;
+  //heroes = HEROES;
+  heroes: Hero[] = [];
 
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-  constructor() { }
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
 }
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,13 @@
 import { Hero } from "./interfaces/heroeInterface";
 
 export const HEROES: Hero[] = [
-  { id: 12, name: 'Dr. Nice' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr. IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 12, name: 'Dr. Nice',power:132,realname:'fssffsd'},
+  { id: 13, name: 'Bombasto',power:124352,realname:'fssffsd' },
+  { id: 14, name: 'Celeritas',power:1432425,realname:'fssffsd' },
+  { id: 15, name: 'Magneta',power:14353,realname:'fssffsd' },
+  { id: 16, name: 'RubberMan',power:1234324,realname:'fssffsd' },
+  { id: 17, name: 'Dynama',power:142343253,realname:'fssffsd' },
+  { id: 18, name: 'Dr. IQ',power:142523,realname:'fssffsd' },
+  { id: 19, name: 'Magma' ,power:125423,realname:'fssffsd'},
+  { id: 20, name: 'Tornado',power:1423524,realname:'fssffsd' }
 ];
